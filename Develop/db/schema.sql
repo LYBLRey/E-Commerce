0f57e1c755b62dcf62987cfca70baa03f32d67a2
Sareybodd Chhorn <sareyboddc@gmail.com>
2021-09-09 14:41:53 -0700

    modified schema adding database models

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -5,3 +5,30 @@ DROP DATABASE IF EXISTS ecommerce_db;
 CREATE DATABASE ecommerce_db;
 
 use ecommerce_db;
+
+CREATE TABLE category (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE product (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(50) NOT NULL,
+    price DECIMAL(20,1) NOT NULL,
+    stock INT(10) NOT NULL MULTIPLEOF,
+    category_id INT,
+    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE
+);
+
+CREATE TABLE tag (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(50)
+);
+
+CREATE TABLE productTag (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE,
+    tag_id INT,
+    CONSTRAINT fk_tag FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE
+);
